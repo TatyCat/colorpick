e319ded6f749123a9c94c24f9a0f6dd0311a143a
TatyCat <williams.tatyanna@gmail.com>
2019-02-23 11:52:13 -0500

    added border around saved color results

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,25 +6,29 @@ class Main extends Component {
       <main>
         <section id="color-results" style={{ backgroundColor: `hsl(0, 0%,0%)` }}></section>
         <article>
-          <p id="slider-results">HSL (39, 100% 4%)</p>
+          <p id="slider-results">HSLa (39, 100%, 4%, 100%)</p>
           <p>Hue</p>
           <section class="slidecontainer">
-            <input type="range" min="1" max="100" value="0" class="slider" id="hue" />
+            <input type="range" min="0" max="100" value="0" class="slider" id="hue" />
           </section>
           <p>Saturation</p>
           <section class="slidecontainer">
-            <input type="range" min="1" max="100" value="0" class="slider" id="saturation" />
+            <input type="range" min="0" max="360" value="0" class="slider" id="saturation" />
           </section>
           <p>Lightness</p>
           <section class="slidecontainer">
-            <input type="range" min="1" max="100" value="0" class="slider" id="lightness" />
+            <input type="range" min="0" max="100" value="50" class="slider" id="lightness" />
+          </section>
+          <p>alpha</p>
+          <section class="slidecontainer">
+            <input type="range" min="0" max="1" value="1" class="slider" id="alpha" />
           </section>
           <button>Store Color</button>
 
           <section id="saved-section">
             <h2>Saved Colors</h2>
-            <div class="saved-color"></div> <p>hsl (208, 100%, 49%)</p>
-            <div class="saved-color"></div> <p>hsl (50, 50%, 10%)</p>
+            <div class="saved-color"></div> <p>hsla (208, 100%, 49%, 100%)</p>
+            <div class="saved-color"></div> <p>hsla (50, 50%, 10%, 100%)</p>
           </section>
         </article>
 
