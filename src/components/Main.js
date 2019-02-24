c9a66ff0704b234824aa3bb78307056c26f05e60
TatyCat <williams.tatyanna@gmail.com>
2019-02-23 21:13:11 -0500

    Completed Sliders Movement, Update of State and Header Update on Change

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,40 +1,80 @@
 import React, { Component } from 'react';
 
 class Main extends Component {
+
+  state = {
+    hue: 0,
+    saturation: 100,
+    lightness: 1,
+    alpha: 1,
+  }
+
+
+  updateHue = (event) => {
+    this.setState({
+      hue: Number(event.target.value)
+    })
+  }
+  updateSaturation = (event) => {
+    this.setState({
+      saturation: Number(event.target.value)
+    })
+  }
+  updateLightness = (event) => {
+    this.setState({
+      lightness: Number(event.target.value)
+    })
+  }
+  updateAlpha = (event) => {
+    this.setState({
+      alpha: Number(event.target.value)
+    })
+  }
+
   render() {
     return (
-      <main>
-        <section id="color-results" style={{ backgroundColor: `hsl(0, 0%,0%)` }}></section>
-        <article>
-          <p id="slider-results">HSLa (39, 100%, 4%, 100%)</p>
-          <p>Hue</p>
-          <section class="slidecontainer">
-            <input type="range" min="0" max="100" value="0" class="slider" id="hue" />
-          </section>
-          <p>Saturation</p>
-          <section class="slidecontainer">
-            <input type="range" min="0" max="360" value="0" class="slider" id="saturation" />
-          </section>
-          <p>Lightness</p>
-          <section class="slidecontainer">
-            <input type="range" min="0" max="100" value="50" class="slider" id="lightness" />
-          </section>
-          <p>alpha</p>
-          <section class="slidecontainer">
-            <input type="range" min="0" max="1" value="1" class="slider" id="alpha" />
-          </section>
-          <button>Store Color</button>
+      <>
+        <main >
+          <section id="color-results" style={{
+            backgroundColor: `hsla(${this.state.hue}, ${this.state.saturation}%, ${this.state.lightness}%, ${this.state.alpha})`
+          }}>
 
-          <section id="saved-section">
-            <h2>Saved Colors</h2>
-            <div class="saved-color"></div> <p>hsla (208, 100%, 49%, 100%)</p>
-            <div class="saved-color"></div> <p>hsla (50, 50%, 10%, 100%)</p>
           </section>
-        </article>
+          <article >
+            <p id="slider-results">HSLa ({this.state.hue}, {this.state.saturation}%,  {this.state.lightness}%, {this.state.alpha})</p>
+            <p>Hue</p>
+            <section class="slidecontainer">
+              <input onChange={this.updateHue} step="1" type="range" min="0" max="360" value={this.state.hue} class="slider" id="hue" />
+            </section>
 
-      </main>
-    );
+            <p>Saturation</p>
+            <section class="slidecontainer">
+              <input onChange={this.updateSaturation} type="range" min="0" max="100" value={this.state.saturation} class="slider" id="saturation" />
+            </section>
+
+            <p>Lightness</p>
+            <section class="slidecontainer">
+              <input onChange={this.updateLightness} type="range" min="0" max="100" value={this.state.lightness} class="slider" id="lightness" />
+            </section>
+
+            <p>alpha</p>
+            <section class="slidecontainer">
+              <input onChange={this.updateAlpha} type="range" step=".1" min="0" max="1" value={this.state.alpha} class="slider" id="alpha" />
+            </section>
+            <button style={{
+              boxShadow: `.2px 1px .3px 6px hsla(${this.state.hue}, ${this.state.saturation}%, ${this.state.lightness}%, ${this.state.alpha}) inset, .2px 1px 1px 3px hsla(${this.state.hue}, ${this.state.saturation}%, ${this.state.lightness}%, ${this.state.alpha})`
+            }}>Store This Color</button>
+
+            <section id="saved-section">
+              <h2>Saved Colors</h2>
+              <div class="saved-color"></div> <p>hsla (208, 100%, 49%, 100%)</p>
+              <div class="saved-color"></div> <p>hsla (50, 50%, 10%, 100%)</p>
+            </section>
+          </article>
+        </main >
+      </>
+    )
   }
 }
 
-export default Main;
+export default Main
